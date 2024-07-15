e50c425a9f898663822000a803cce020ccc79895
Pratiksha-Auti <pratikshaauti004@gmail.com>
2024-07-15 20:27:30 +0530

    Datatypes Codes

---
+++ 2_Datatypes/program2.java
@@ -0,0 +1,11 @@
+class demo{
+	public static void main(String[] args){
+       int  var1=18;
+       int  var2=18;
+System.out.println(var1);
+System.out.println(var2);
+var1=var1+var2;
+System.out.println(var1);
+System.out.println(var2);
+}
+}
